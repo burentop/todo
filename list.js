87546a95f2b2f30574f18dbf2d1596cae1e9723d
Brent Perry <burentoperii@gmail.com>
2017-05-04 11:38:51 -0500

    complete basic functionality

--- list.js
+++ list.js
@@ -1,1 +1,20 @@
-console.log("Connected");
+var todos = ["Buy new turtle"];
+
+var input = prompt("What would you like to do?");
+
+
+
+while(input !== "quit") {
+	// hand input
+	if (input === "list") {
+		console.log(todos);
+	} else if (input === "new") {
+		// ask for new todo
+		var newTodo = prompt("Enter new todo");
+		// add to todos array
+		todos.push(newTodo);
+	}
+	//ask again for new input
+	input = prompt("What would you like to do?");
+}
+console.log("Ok, you quit the app");
