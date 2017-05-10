d438cfdbdad1259c036c46a6a39b54bf682e78e8
Brent Perry <burentoperii@gmail.com>
2017-05-10 12:13:00 -0500

    complete styling and functionality

--- list.js
+++ list.js
@@ -7,14 +7,37 @@ var input = prompt("What would you like to do?");
 while(input !== "quit") {
 	// hand input
 	if (input === "list") {
-		console.log(todos);
+		listTodos();
 	} else if (input === "new") {
-		// ask for new todo
-		var newTodo = prompt("Enter new todo");
-		// add to todos array
-		todos.push(newTodo);
+		addTodo();
+	} else if (input === "delete") {
+		deleteTodo();
 	}
 	//ask again for new input
 	input = prompt("What would you like to do?");
 }
 console.log("Ok, you quit the app");
+
+function listTodos() {
+	console.log("**********");
+	todos.forEach(function(todo, i) {
+		console.log(i + ": " + todo);
+	});
+	console.log("**********");
+}
+
+function addTodo() {
+	// ask for new todo
+	var newTodo = prompt("Enter new todo");
+	// add to todos array
+	todos.push(newTodo);
+	console.log("Added todo");
+}
+
+function deleteTodo() {
+	//ask for index
+	var index = prompt("Enter index of todo to delete");
+	//delete todo
+	todos.splice(index, 1);
+	console.log("Deleted todo");
+}
